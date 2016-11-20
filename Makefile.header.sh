bd7ea965b0cd0655135601323e74499528c2e90a
Brice Burgess <nesta@iceburg.net>
2016-11-20 10:40:53 -0500

    package a-la-care helpers at get.iceburg.net

--- Makefile.header.sh
+++ Makefile.header.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
 #
 # shell-helpers version @VERSION@ build @BUILD@
-# https://github.com/briceburg/shell-helpers
-# copyright 2016-@YEAR@ Brice Burgess - Licensed under the Apache License 2.0
+#   https://github.com/briceburg/shell-helpers
+# Copyright 2016-present Brice Burgess, Licensed under the Apache License 2.0
+#
+# @ICEBURG_UPDATE_URL=@UPDATE_URL@
 #
