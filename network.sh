8b6883fb2b0a30c134f72c57642398467dd22d0d
Brice Burgess <nesta@iceburg.net>
2016-12-02 22:18:36 -0500

    print the nets

--- network.sh
+++ network.sh
@@ -1,11 +1,8 @@
 # shell-helpers - a series of tubes and pipes provided by al gore
 #   https://github.com/briceburg/shell-helpers
 
-
 # usage: network/fetch <url> <target> [force boolean]
 network/fetch(){
-  local wget=${WGET_PATH:-wget}
-  local curl=${CURL_PATH:-curl}
   local url="$1"
   local target="$2"
   local force=${3:-false}
@@ -15,13 +12,23 @@ network/fetch(){
   }
   rm -rf "$target"
 
+  network/print "$url" > "$target"
+  [ -e $target ]
+}
+
+# usage: network/print <url>
+# similar to network/fetch but prints a URL to stdout
+network/print(){
+  local url="$1"
+  local wget=${WGET_PATH:-wget}
+  local curl=${CURL_PATH:-curl}
+
   if is/cmd $wget ; then
-    $wget -qO "$target" $url || { rm -rf "$target" ; }
+    $wget -qO - $url
   elif is/cmd $curl ; then
-    $curl -Lfso "$target" $url
+    $curl -Lfs $url
   else
     io/warn "unable to fetch $url" "missing both curl and wget"
+    return 1
   fi
-
-  [ -e $target ]
 }
