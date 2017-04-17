c237a1398a6c746bd913cf67bc0fe73c1ddd676d
Brice Burgess <nesta@iceburg.net>
2017-04-17 13:47:44 -0400

    support inclusion of shell-helpers in pipefail environments

--- Makefile.footer.sh
+++ Makefile.footer.sh
@@ -1,8 +1,8 @@
 #
 # allow usage as a shebang
 #
-[ "$0" = "${BASH_SOURCE[0]}" ] && [ ! $# -eq 0 ] && {
+if [[ "$0" = "${BASH_SOURCE[0]}" && ! $# -eq 0 ]]; then
   __shparent="$1"
   shift
   . "$__shparent"
-}
+fi
